25d72fab35d2b099e745994e8024a2f663a1ae39
Pallav Regmi <119284387+PallavRegmi@users.noreply.github.com>
2023-03-05 19:22:15 -0700

    Update soduku.c

--- soduku.c
+++ soduku.c
@@ -1,4 +1,3 @@
-pregmi@setebos:~/cs241/lab04$ gcc -Wall -ansi -pedantic sudoku.c
 /*Pallav Regmi*/
 /*CS241-2DK*/
 /*Lab04*/
@@ -59,5 +58,152 @@ int solve(int i, int j) {
     for (k = 0; k < N; k++) {
         if (!row[i][k] && !col[j][k] && !box[(i/3)*3 + j/3][k]) {
             sudoku[i][j] = k + '1';
--UU-:----F1  sudoku.c       Top L1     (C/*l Abbrev) -------------------------------------------------------------------------------------------------------------------------------------------------------
-For information about GNU Emacs and the GNU system, type C-h C-a.
+            row[i][k] = 1;
+            col[j][k] = 1;
+            box[(i/3)*3 + j/3][k] = 1;
+
+            if (j == N-1) {
+                if (solve(i+1, 0)) {
+                    return 1;
+                }
+            } else {
+                if (solve(i, j+1)) {
+                    return 1;
+                }
+            }
+
+            row[i][k] = 0;
+            col[j][k] = 0;
+            box[(i/3)*3 + j/3][k] = 0;
+            sudoku[i][j] = UNASSIGNED;
+        }
+    }
+
+    return 0;
+}
+
+/*Function to print the solved Sudoku puzzle*/
+void printSudoku() {
+    int i, j;
+    printf("\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%c", sudoku[i][j]);
+        }
+
+    }
+}
+
+void check_duplicate(char sudoku[N][N],int *x) {
+  int i,j,k;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            char current = sudoku[i][j];
+
+            /*Skip checking if the character is "."*/
+            if (current == '.') {
+                continue;
+            }
+
+            /*Check for duplicates in the row*/
+            for (k = j+1 ; k < N; k++) {
+
+
+                if (sudoku[i][k] == current && sudoku[i][k] != '.') {
+                    printf("\nError\n");
+                    *x=1;
+                    return;
+                }
+            }
+
+            /*Check for duplicates in the column*/
+
+            for ( k = i + 1; k < N; k++) {
+                if (sudoku[k][j] == current && sudoku[k][j] != '.') {
+                    printf("\nError\n");
+                    *x=1;
+                    return;
+                }
+            }
+        }
+    }
+}
+
+void check_array(char arr[], int size, int* y) {
+    int i;
+    for (i = 0; i < size; i++) {
+        char current = arr[i];
+        if ((current < '0' || current > '9') && current != '.') {
+            printf("\nError\n");
+            *y=1;
+            return;
+        }
+    }
+}
+
+int main() {
+    while(true){
+      int i,x,y;
+
+
+        char *arr = NULL; /*initialize pointer to NULL*/
+        int size = 0;
+        int capacity = 0; /*current capacity of the array*/
+
+
+
+        char ch;
+
+        while ((ch = getchar()) != '\n') {
+            if (size == capacity) {
+                capacity += CHUNK_SIZE;
+                arr = (char *) realloc(arr, capacity * sizeof(char));
+            }
+
+            arr[size++] = ch;
+        }
+
+
+        printf("\n");
+        for (i = 0; i < size; i++) {
+            printf("%c", arr[i]);
+        }
+        /*check if the arrary has anything other than numbers & "."s.*/
+        y=0;
+        check_array(arr, size, &y);
+        if(y!=1){
+            /*Check if the number of characters entered is 81*/
+            if (size != 81) {
+                printf("\nError\n");
+           }else{
+
+                /*Take input of the Sudoku puzzle*/
+                int index = 0;
+                int i,j;
+                for (i = 0; i < N; i++) {
+                  for ( j = 0; j < N; j++) {
+                        sudoku[i][j] = *(arr + index);
+                        index++;
+                    }
+                }
+
+                free(arr); /* free the memory allocated*/
+                /*checking for duplicates*/
+                x=0;
+                check_duplicate(sudoku,&x);
+                if(x!=1){
+                    /*Initialize the row, col, and box arrays*/
+                    initialize();
+                    /*Solve the Sudoku puzzle*/
+                    if (solve(0, 0)) {
+                        printSudoku();
+                        printf("\n");
+                    } else {
+                        printf("\nNo solution found.\n");
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
